dbb78334d796b7cdcc2a61bedff598e7387ce8d4
Baltazar Servín <thebaltazarsr@gmail.com>
2025-10-28 16:43:36 -0600

    Initial Commit

---
+++ CovidChecker/CovidCheckerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CovidCheckerApp.swift
+//  CovidChecker
+//
+//  Created by Baltazar Servin on 28/10/25.
+//
+
+import SwiftUI
+
+@main
+struct CovidCheckerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CovidCheckerTests/CovidCheckerTests.swift
@@ -0,0 +1,17 @@
+//
+//  CovidCheckerTests.swift
+//  CovidCheckerTests
+//
+//  Created by Baltazar Servin on 28/10/25.
+//
+
+import Testing
+@testable import CovidChecker
+
+struct CovidCheckerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
